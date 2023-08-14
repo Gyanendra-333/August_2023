05856671951f879ce8778ecf014d501f631e730b
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-08-14 07:29:50 +0530

    linear search

--- Searching_Algorithms/Linear_Search/linear-search.js
+++ Searching_Algorithms/Linear_Search/linear-search.js
@@ -10,4 +10,26 @@ let n = arr.length;
 let x = 20;
 
 let result = linearSearch(arr, n, x);
-(result == -1 ? console.log("not found") : console.log("found index : " + result));
+(result == -1 ? console.log("not found : ") : console.log("index found : ") + result);
+
+
+// Javascript code to linearly search x in arr[].
+
+// function search(arr, n, x) {
+//     for (let i = 0; i < n; i++)
+//         if (arr[i] == x)
+//             return i;
+//     return -1;
+// }
+
+
+// let arr = [2, 3, 4, 10, 40];
+// let x = 2;
+// let n = arr.length;
+
+// // Function call
+// let result = search(arr, n, x);
+// (result == x)
+//     ? console.log("Element is not present in array")
+//     : console.log("Element is present at index " + result);
+
