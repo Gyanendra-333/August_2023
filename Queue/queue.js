545976bae73367f674a5c806ccc39b79ad11b7ba
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-08-26 07:25:52 +0530

    queue

--- Queue/queue.js
+++ Queue/queue.js
@@ -3,22 +3,26 @@
 
 class Queue {
     // Array is used to implement a Queue
+
     constructor() {
         this.items = [];
     }
     isEmpty() {
         // return true if the queue is empty.
+
         return this.items.length == 0;
     }
     enqueue(element) {
         // adding element to the queue
+
         this.items.push(element);
-        document.write(element + " enqueued to queue<br>");
+        console.log(element + " enqueued to queue");
     }
     dequeue() {
         // removing element from the queue
         // returns underflow when called
         // on empty queue
+
         if (this.isEmpty())
             return "Underflow<br>";
         return this.items.shift();
@@ -26,20 +30,23 @@ class Queue {
     front() {
         // returns the Front element of
         // the queue without removing it.
+
         if (this.isEmpty())
-            return "No elements in Queue<br>";
+            return "No elements in Queue";
         return this.items[0];
     }
     rear() {
         // returns the Rear element of
         // the queue without removing it.
+
         if (this.isEmpty())
-            return "No elements in Queue<br>";
+            return "No elements in Queue";
         return this.items[this.items.length - 1];
     }
 }
 
 // creating object for queue class
+
 var queue = new Queue();
 
 // Adding elements to the queue
@@ -50,12 +57,13 @@ queue.enqueue(40);
 
 // queue contains [10, 20, 30, 40]
 // removes 10
-document.write(queue.dequeue() + " dequeued from queue<br>");
+
+console.log(queue.dequeue() + " dequeued from queue");
 
 // queue contains [20, 30, 40]
 // Front is now 20
-document.write("Front item is " + queue.front() + "<br>");
+console.log("Front item is " + queue.front() + "");
 
 // printing the rear element
 // Rear is 40
-document.write("Rear item is " + queue.rear() + "<br>");
+console.log("Rear item is " + queue.rear() + "");
