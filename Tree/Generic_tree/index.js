4a929edd5e6f5d3611c876981cfdc14342fa818a
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-08-28 09:49:42 +0530

    Generic tree

---
+++ Tree/Generic_tree/index.js
@@ -0,0 +1,22 @@
+// class Node {
+//     constructor(data) {
+//         this.data = data;
+//         this.children = [];
+//     }
+// }
+
+
+
+
+// Javascript code for above approach
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.firstchild = null;
+        this.secondchild = null;
+        this.thirdchild = null;
+        this.fourthchild = null;
+        this.fifthchild = null;
+        this.sixthchild = null;
+    }
+}
