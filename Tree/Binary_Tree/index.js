36865735fabfb98d44091c7fee04e7b7c6bbf4a1
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-08-31 06:17:09 +0530

    bst

--- Tree/Binary_Tree/index.js
+++ Tree/Binary_Tree/index.js
@@ -9,51 +9,136 @@
 
 
 
-/* Class containing left and right child of current
+// /* Class containing left and right child of current
 
-    node and key value*/
+//     node and key value*/
+// class Node {
+
+//     constructor(val) {
+//         this.key = val;
+//         this.left = null;
+//         this.right = null;
+//     }
+
+// }
+// // A javascript program to introduce Binary Tree
+// // Root of Binary Tree
+
+// var root = null;
+
+// /*create root*/
+
+// root = new Node(1);
+
+// /* following is the tree after above statement
+// 1
+// / \
+// null null */
+
+// root.left = new Node(2);
+
+// root.right = new Node(3);
+
+// /* 2 and 3 become left and right children of 1
+// 1
+// / \
+// 2 3
+// / \ / \
+// null null null null */
+
+// root.left.left = new Node(4);
+
+// /* 4 becomes left child of 2
+// 1
+// / \
+// 2 3
+// / \ / \
+// 4 null null null
+// / \
+// null null
+// */
+
+
+
+
+
+
+
+// javascript program to insert a node
+// in a BST
+
+// Given Node
 class Node {
 
-    constructor(val) {
-        this.key = val;
+    constructor(key) {
+        this.key = key;
         this.left = null;
         this.right = null;
     }
 
 }
-// A javascript program to introduce Binary Tree
-// Root of Binary Tree
 
-var root = null;
 
-/*create root*/
+// Function to insert a new node with
+// given key in BST
+function insert(node, key) {
 
-root = new Node(1);
+    // If the tree is empty, return a new node
+    if (node == null)
+        return new Node(key);
 
-/* following is the tree after above statement
-1
-/ \
-null null */
+    // Otherwise, recur down the tree
+    if (key < node.key) {
+        node.left = insert(node.left, key);
+    }
+    else if (key > node.key) {
+        node.right = insert(node.right, key);
+    }
 
-root.left = new Node(2);
+    // Return the node pointer
+    return node;
+}
 
-root.right = new Node(3);
+// Function to do inorder traversal of BST
+function inorder(root) {
+    if (root != null) {
+        inorder(root.left);
+        document.write(root.key + " ");
+        inorder(root.right);
+    }
+}
 
-/* 2 and 3 become left and right children of 1
-1
-/ \
-2 3
-/ \ / \
-null null null null */
 
-root.left.left = new Node(4);
-
-/* 4 becomes left child of 2
-1
-/ \
-2 3
-/ \ / \
-4 null null null
-/ \
-null null
+/* Let us create following BST
+        50
+    /	 \
+    30	 70
+    / \ / \
+20 40 60 80
 */
+let root = null;
+
+// Inserting value 50
+root = insert(root, 50);
+
+// Inserting value 30
+root = insert(root, 30);
+
+// Inserting value 20
+root = insert(root, 20);
+
+// Inserting value 40
+root = insert(root, 40);
+
+// Inserting value 70
+root = insert(root, 70);
+
+// Inserting value 60
+root = insert(root, 60);
+
+// Inserting value 80
+root = insert(root, 80);
+
+// Print the BST
+inorder(root);
+
