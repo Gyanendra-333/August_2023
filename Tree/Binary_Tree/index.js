597ce83126c8f42f72719b999d863eba6a09700b
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-08-31 06:10:13 +0530

    revision

--- Tree/Binary_Tree/index.js
+++ Tree/Binary_Tree/index.js
@@ -1,6 +1,59 @@
+// class Node {
+//     constructor(item) {
+//         this.key = item;
+//         this.left = this.right = null;
+//     }
+// }
+
+
+
+
+
+/* Class containing left and right child of current
+
+    node and key value*/
 class Node {
-    constructor(item) {
-        this.key = item;
-        this.left = this.right = null;
+
+    constructor(val) {
+        this.key = val;
+        this.left = null;
+        this.right = null;
     }
+
 }
+// A javascript program to introduce Binary Tree
+// Root of Binary Tree
+
+var root = null;
+
+/*create root*/
+
+root = new Node(1);
+
+/* following is the tree after above statement
+1
+/ \
+null null */
+
+root.left = new Node(2);
+
+root.right = new Node(3);
+
+/* 2 and 3 become left and right children of 1
+1
+/ \
+2 3
+/ \ / \
+null null null null */
+
+root.left.left = new Node(4);
+
+/* 4 becomes left child of 2
+1
+/ \
+2 3
+/ \ / \
+4 null null null
+/ \
+null null
+*/
